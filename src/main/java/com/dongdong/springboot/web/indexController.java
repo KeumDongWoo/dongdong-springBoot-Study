b7e8c11b52b4eaecf994788f4ded53a6147c140e
geumdong-u <seletr96@gmail.com>
2020-03-17 23:53:17 +0900

    수정기능 추가

--- src/main/java/com/dongdong/springboot/web/indexController.java
+++ src/main/java/com/dongdong/springboot/web/indexController.java
@@ -1,10 +1,12 @@
 package com.dongdong.springboot.web;
 
 import com.dongdong.springboot.service.posts.PostsService;
+import com.dongdong.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class indexController {
     public String index(Model model){   //서버 템플릿 엔진에서 사용할수 있는 객체를 저장
         model.addAttribute("posts",postsService.findAllDesc());//findAllDesc() 결과를 posts 로 index.mustache에 전달
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id , Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 }
